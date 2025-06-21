bc1f06118605e759138c746f66cfac554a60d947
DougSantos <dougvsdoug01@gmail.com>
2025-06-21 15:56:52 -0400

    Entity 3 done

--- server/models/payment.js
+++ server/models/payment.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const paymentSchema = new Schema({
+const paymentSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
